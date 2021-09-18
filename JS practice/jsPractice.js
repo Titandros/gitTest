e5aa0c03a8f5e947ba3845d5a3a18c8ad6789f0d
Titandros <olvi0088@gmail.com>
2021-09-18 11:48:03 +0300

    Practice JS 1

---
+++ JS practice/jsPractice.js
@@ -0,0 +1,16 @@
+const camelCase = 0;
+const snake_case = 1;
+const PascalCase = 2;
+
+//alert() останавливает все взаимодействие с браузером до тех пор, пока сообщение не будет отклонено, 
+//в то время как console.log() просто выводит сообщение на консоль.
+
+//Например, использую alert() необходимо закрывать каждый раз окно вывода результата, чтобы увидеть вывод следующего
+
+console.log(camelCase);
+console.log(snake_case);
+console.log(PascalCase);
+
+alert(camelCase);
+alert(snake_case);
+alert(PascalCase);
